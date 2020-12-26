680118fbf4d948ff5f7334b0a9cc3be89f98b680
Michael Taggart <mtaggarthawk@gmail.com>
2020-12-26 11:45:15 -0700

    working code?

---
+++ app-3/src/components/PokemonList.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const PokemonList = (props) => {
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
+            setList(res.data.results)
+        })
+    }, [])
+
+    return (
+        <div>
+            {list.map((pokemon) => {
+                return (
+                    <Link key={pokemon.url} to={`/pokemon/${pokemon.nam}`}>
+                        <h2>{pokemon.name}</h2>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+export default PokemonList
